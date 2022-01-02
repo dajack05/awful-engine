15218fcc64d18d013a55bd812c684b226748f292
Daniel Jackson <dajack05@gmail.com>
2022-01-01 22:15:37 -0500

    update

--- src/awful/macos/Input.c
+++ src/awful/macos/Input.c
@@ -3,11 +3,32 @@
 #ifdef __MACOS__
 
 #include <awful/Input.h>
-#include <stdio.h>
+#include <termios.h>
+#include <unistd.h>
 
 int InputGetChar() {
-  int c = getchar();
-  return c;
+  // fd = sys.stdin.fileno()
+  int ttyDevice = STDOUT_FILENO;
+  struct termios old_settings;
+  struct termios tty;
+  // old_settings = termios.tcgetattr(fd)
+  tcgetattr(ttyDevice, &old_settings);
+  // try:
+  //     tty.setraw(sys.stdin.fileno())
+  cfmakeraw(&tty);
+  //     start_pos = sys.stdin.buffer.tell()
+  //     seq = sys.stdin.read(1)
+  //     end_pos = sys.stdin.buffer.tell()
+  //     seq_length = end_pos - start_pos
+  //     if seq_length < 1:
+  //         return ''
+  //     seq += sys.stdin.read(seq_length-1)
+  //     return seq
+  // except KeyboardInterrupt:
+  //     print("\b\b  ")
+  //     pass
+  // finally:
+  //     termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
 }
 
 #endif

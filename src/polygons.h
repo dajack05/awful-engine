d43517a8569b4d4c873d3787480b425f800587d6
Daniel Jackson <dajack05@gmail.com>
2022-02-04 11:02:09 -0500

    update

--- src/polygons.h
+++ src/polygons.h
@@ -12,16 +12,14 @@ void Run() {
 
   struct WindowSize winSize = DisplayInit(false);
 
-  CgmVec3 v1 = CgmVec3_init(15.0F, 20.0F, 0.0F);
-  CgmVec3 v2 = CgmVec3_init(50.0F, 30.0F, 0.0F);
+  CgmVec3 v1 = CgmVec3_init(20.0F, 20.0F, 20.0F);
+  CgmVec3 v2 = CgmVec3_init(50.0F, 20.0F, 40.0F);
 
   CgmMat4x4 proj;
   CgmMat4x4_perspective(&proj, 90.0F, 1.0F, 0.01F, 100.0F);
 
   CgmMat4x4 view;
   CgmVec3 cameraPos = CgmVec3_init(0.0F, 0.0F, 0.0F);
-
-  bool persp = false;
 
   bool should_run = true;
   double t = 0.0;
@@ -32,48 +30,37 @@ void Run() {
     // v1.x = 15.0F + sin(t) * 10.0F;
     // v1.z = 15.0F + cos(t) * 10.0F;
 
-    CgmVec3 nv1 = CgmVec3_init_even(0);
-    CgmVec3 nv2 = CgmVec3_init_even(0);
+    CgmVec4 nv1 = CgmVec4_init(1, 1, 1, 1);
+    CgmVec4 nv2 = CgmVec4_init(1, 1, 1, 1);
 
-    cameraPos.z = -10.0F + (float)sin(t) * 10.0F;
+    // cameraPos.z = -10.0F + (float)sin(t) * 10.0F;
+    cameraPos.z += 1.0F;
 
     CgmMat4x4_identity(&view);
     CgmMat4x4_translate(&view, cameraPos);
 
     DisplayClear();
 
-    if (persp) {
-      nv1 = CgmMat4x4_mul_point(&view, v1);
-      nv1 = CgmMat4x4_mul_point(&proj, nv1);
+    nv1 = CgmMat4x4_mul_point4(&view, CgmVec4_init(v1.x, v1.y, v1.z, 1));
+    nv1 = CgmMat4x4_mul_point4(&proj, nv1);
 
-      nv2 = CgmMat4x4_mul_point(&view, v2);
-      nv2 = CgmMat4x4_mul_point(&proj, nv2);
+    nv2 = CgmMat4x4_mul_point4(&view, CgmVec4_init(v2.x, v2.y, v2.z, 1));
+    nv2 = CgmMat4x4_mul_point4(&proj, nv2);
 
-      DisplaySetStr(0, 9, "True");
-    } else {
-      nv1 = v1;
-      nv2 = v2;
-      DisplaySetStr(0, 9, "False");
-    }
+    CgmVec2 p1 =
+        CgmVec2_init((int)((nv1.x / nv1.w + 1) * (winSize.width / 2)),
+                     (int)((1 - nv1.y / nv1.w) * (winSize.height / 2)));
 
-    DisplaySetFloat(0, 8, nv1.y);
+    CgmVec2 p2 =
+        CgmVec2_init((int)((nv2.x / nv2.w + 1) * (winSize.width / 2)),
+                     (int)((1 - nv2.y / nv2.w) * (winSize.height / 2)));
 
-    nv1.x = nv1.x / nv1.z;
-    nv1.y = nv1.y / nv1.z;
-
-    nv2.x = nv2.x / nv2.z;
-    nv2.y = nv2.y / nv2.z;
-
-    DrawLine(CgmVec2_init(nv1.x, nv1.y), CgmVec2_init(nv2.x, nv2.y));
+    DrawLine(p1, p2);
 
     DisplayPresent();
 
     if (isKeyDown('q')) {
       should_run = false;
-    }
-
-    if (isKeyDown('f')) {
-      persp = !persp;
     }
   }
 }

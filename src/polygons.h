0f6860527008686f5211bb46d907433fdbd87b9f
Daniel Jackson <dajack05@gmail.com>
2022-02-04 13:07:05 -0500

    It's alive!!!

--- src/polygons.h
+++ src/polygons.h
@@ -8,18 +8,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+CgmVec2 transformPoint(CgmMat4x4 *model, CgmMat4x4 *view, CgmMat4x4 *projection,
+                       CgmVec3 point, struct WindowSize *winSize) {
+  CgmVec4 newPoint = CgmVec4_init(point.x, point.y, point.z, 0);
+
+  newPoint = CgmMat4x4_mul_point4(model, newPoint);
+  newPoint = CgmMat4x4_mul_point4(view, newPoint);
+  newPoint = CgmMat4x4_mul_point4(projection, newPoint);
+
+  return CgmVec2_init(
+      (int)((newPoint.x / newPoint.w + 1) * (winSize->width / 2)),
+      (int)((1 - newPoint.y / newPoint.w) * (winSize->height / 2)));
+}
+
 void Run() {
 
   struct WindowSize winSize = DisplayInit(false);
 
-  CgmVec3 v1 = CgmVec3_init(20.0F, 20.0F, 20.0F);
-  CgmVec3 v2 = CgmVec3_init(50.0F, 20.0F, 40.0F);
+  // Front Face
+  CgmVec3 v11 = CgmVec3_init(-05, -05, -05);
+  CgmVec3 v12 = CgmVec3_init(+05, -05, -05);
+  CgmVec3 v13 = CgmVec3_init(+05, +05, -05);
+  CgmVec3 v14 = CgmVec3_init(-05, +05, -05);
+
+  CgmVec3 v21 = CgmVec3_init(-05, -05, +05);
+  CgmVec3 v22 = CgmVec3_init(+05, -05, +05);
+  CgmVec3 v23 = CgmVec3_init(+05, +05, +05);
+  CgmVec3 v24 = CgmVec3_init(-05, +05, +05);
 
   CgmMat4x4 proj;
-  CgmMat4x4_perspective(&proj, 90.0F, 1.0F, 0.01F, 100.0F);
+  CgmMat4x4_perspective(&proj, 45.0F,
+                        (float)winSize.width / 2 / (float)winSize.height, 0.01F,
+                        100.0F);
 
   CgmMat4x4 view;
-  CgmVec3 cameraPos = CgmVec3_init(0.0F, 0.0F, 0.0F);
+  CgmMat4x4_identity(&view);
+  CgmMat4x4_translate(&view, CgmVec3_init(0, 0, 20));
+
+  CgmMat4x4 model;
+  CgmMat4x4_identity(&model);
 
   bool should_run = true;
   double t = 0.0;
@@ -27,35 +54,36 @@ void Run() {
     pollInput();
     t += 0.1;
 
-    // v1.x = 15.0F + sin(t) * 10.0F;
-    // v1.z = 15.0F + cos(t) * 10.0F;
+    // CgmMat4x4_translate(&model, CgmVec3_init(0.0F, 0.0F, 0.2F));
+    CgmMat4x4_rotate(&model, CgmVec3_init(0, 1, 0), 0.1F);
 
-    CgmVec4 nv1 = CgmVec4_init(1, 1, 1, 1);
-    CgmVec4 nv2 = CgmVec4_init(1, 1, 1, 1);
+    // Transform Points
+    CgmVec2 p11 = transformPoint(&model, &view, &proj, v11, &winSize);
+    CgmVec2 p12 = transformPoint(&model, &view, &proj, v12, &winSize);
+    CgmVec2 p13 = transformPoint(&model, &view, &proj, v13, &winSize);
+    CgmVec2 p14 = transformPoint(&model, &view, &proj, v14, &winSize);
 
-    // cameraPos.z = -10.0F + (float)sin(t) * 10.0F;
-    cameraPos.z += 1.0F;
-
-    CgmMat4x4_identity(&view);
-    CgmMat4x4_translate(&view, cameraPos);
+    CgmVec2 p21 = transformPoint(&model, &view, &proj, v21, &winSize);
+    CgmVec2 p22 = transformPoint(&model, &view, &proj, v22, &winSize);
+    CgmVec2 p23 = transformPoint(&model, &view, &proj, v23, &winSize);
+    CgmVec2 p24 = transformPoint(&model, &view, &proj, v24, &winSize);
 
     DisplayClear();
 
-    nv1 = CgmMat4x4_mul_point4(&view, CgmVec4_init(v1.x, v1.y, v1.z, 1));
-    nv1 = CgmMat4x4_mul_point4(&proj, nv1);
+    DrawLine(p11, p12);
+    DrawLine(p12, p13);
+    DrawLine(p13, p14);
+    DrawLine(p14, p11);
 
-    nv2 = CgmMat4x4_mul_point4(&view, CgmVec4_init(v2.x, v2.y, v2.z, 1));
-    nv2 = CgmMat4x4_mul_point4(&proj, nv2);
+    DrawLine(p21, p22);
+    DrawLine(p22, p23);
+    DrawLine(p23, p24);
+    DrawLine(p24, p21);
 
-    CgmVec2 p1 =
-        CgmVec2_init((int)((nv1.x / nv1.w + 1) * (winSize.width / 2)),
-                     (int)((1 - nv1.y / nv1.w) * (winSize.height / 2)));
-
-    CgmVec2 p2 =
-        CgmVec2_init((int)((nv2.x / nv2.w + 1) * (winSize.width / 2)),
-                     (int)((1 - nv2.y / nv2.w) * (winSize.height / 2)));
-
-    DrawLine(p1, p2);
+    DrawLine(p11, p21);
+    DrawLine(p12, p22);
+    DrawLine(p13, p23);
+    DrawLine(p14, p24);
 
     DisplayPresent();
 
